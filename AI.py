3e1756c40c9d50da634c504c02eaededef7e5019
Zelcus <zelcus@hotmail.com>
2018-04-18 13:19:04 +0200

    LCk pull
    Dopeness is the friend

--- AI.py
+++ AI.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 data = pd.read_csv("Test.txt")
+
+
 print (data.head())
 
 
@@ -10,18 +12,19 @@ names = ["league", "Date", "Blue Team", "Red Team", "FTBT", "FTRT", "FTR", "GBT"
 
 data = pandas.read_csv(filename, names=names)
 '''
-
+#FTR works as our label the rest is our features.
 y = data.FTR
 X = data.drop("FTR", axis = 1)
 
 
+
 #test_size = the amount of test-cases we would like.
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size= .2)
+
+
+
 '''
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2)
-
-
-
-
 print()
 print ("\nX_train:\n")
 print(X_train.head())
@@ -31,7 +34,10 @@ print(X_test.head())
 '''
 
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
-print (clf.predict([[1, 3]]))
+clf = clf.fit(X_train, y_train)
+#Blue Team with ID X vs Red Team with ID Y.
+predictions = clf.predict(X_test)
+print(predictions)
 
-
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, predictions))
